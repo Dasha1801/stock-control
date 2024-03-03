42d036275f78182ab532e0a2ce23b16bbb4b939a
Darya Belskaya <dariya.b@itfox-web.com>
2024-03-03 20:24:53 +0300

    add: table, select components

--- src/components/list-products/index.tsx
+++ src/components/list-products/index.tsx
@@ -1,33 +1,52 @@
-import { useNavigate } from "react-router-dom"
+import { Table } from "antd"
 import { useAppSelector } from "../../redux/store"
-import './style.scss'
-import { allPaths } from "../../router/app-router"
 
-const heading = ['наименование', 'код', 'каталог', 'описание', 'бренд', 'цена', 'количество']
+
+const columns = [
+  {
+    title: 'наименование',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'код',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'каталог',
+    dataIndex: 'catalog',
+    key: 'catalog',
+  },
+  {
+    title: 'описание',
+    dataIndex: 'description',
+    key: 'description',
+  },
+  {
+    title: 'бренд',
+    dataIndex: 'brand',
+    key: 'brand',
+  },
+  {
+    title: 'цена',
+    dataIndex: 'price',
+    key: 'price',
+  },
+  {
+    title: 'количество',
+    dataIndex: 'price',
+    key: 'quantity',
+  },
+]
 
 
 const ListCatalogs = () => {
-  const navigate = useNavigate()
-  const { catalogs, nomenclatures } = useAppSelector((store) => store.data)
-  console.log(nomenclatures[0])
+  const { nomenclatures } = useAppSelector((store) => store.data)
   return (
     <div className="wrapper-table">
-      <table>
-        <thead>
-          {heading.map((el, idx) => <th key={`item-head-${idx}`}>{el}</th>)}
-        </thead>
-        <tbody>
-          {nomenclatures.map((el) => <tr onClick={() => navigate(`${allPaths.catalog.path}/${el.id}`)} className="active" key={el.id}>
-            <td>{el.name}</td>
-            <td>{el.id}</td>
-            <td>{el.catalog}</td>
-            <td>{el.description}</td>
-            <td>{el.brand}</td>
-            <td>{el.price}</td>
-            <td>{el.quantity}</td>
-          </tr>)}
-        </tbody>
-      </table></div>)
+      <Table dataSource={nomenclatures} columns={columns} />
+    </div>)
 }
 
 export default ListCatalogs
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,13 +1,15 @@
+import Actions from "../../components/actions"
 import ListProducts from "../../components/list-products"
 import UploadFile from "../../components/upload-file"
 import { useAppSelector } from "../../redux/store"
+import './style.scss'
 
 const Main = () => {
   const { catalogs } = useAppSelector((store) => store.data)
 
   return (
     <>{
-      catalogs.length ? <ListProducts /> : <UploadFile />
+      catalogs.length ? <div className="wrapper-main"><Actions /><ListProducts /></div> : <UploadFile />
     }
     </>
   )
